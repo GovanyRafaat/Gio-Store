c60729144f3c43159b6680986e6b16a0e6ead7bd
GovanyRafaat <govanyrafaat64@gmail.com>
2026-05-09 15:30:34 +0300

    LS1

--- products-data.js
+++ products-data.js
@@ -34,7 +34,79 @@ window.PRODUCTS = [
     name: "ايربودز وساعة ديزني",
     price: 750,
     category: "إلكترونيات",
-    description: "بدور على هدية شيك وكيوت؟ طقم ايربودز وساعة ديزني هيخطف قلب أي بنت من أول نظرة! تصميم كارتوني تحفة وسماعة لاسلكية بجودة صوت عالية وراحة وأناقة في نفس الوقت!– الهدية الأجمل لكل بنت بتحب الكيوت! 🎀🎧⌚"
+    description: "بتدور على هدية شيك وكيوت؟ طقم ايربودز وساعة ديزني هيخطف قلب أي بنت من أول نظرة! تصميم كارتوني تحفة وسماعة لاسلكية بجودة صوت عالية وراحة وأناقة في نفس الوقت!– الهدية الأجمل لكل بنت بتحب الكيوت! 🎀🎧⌚"
+  },
+  {
+    images: [
+      "https://media.taager.com/9743c6ef-f77a-4f0e-8f58-16dc4a52f253.jpg",
+      "https://media.taager.com/0932b115-1be5-4689-a003-9a63e5e36fcd.jpg",
+      "https://media.taager.com/e412aeb8-f512-4ed0-a729-1419f1bfc628.jpg",
+      "https://media.taager.com/d0066590-9b9e-49f2-b53d-f920dfdacf4d.jpg"
+    ],
+    name: "قلم SHI-GLAM للاظافر في 7 ايام",
+    price: 250,
+    category: "صحة وجمال",
+    description: "قلم SHI-GLAM يقوي الأظافر ويقلل التكسر والجفاف بتركيبة طبيعية غنية بالبيوتين وأوميغا-3، ويمنحك أظافر قوية ولامعة خلال 7 أيام بسهولة وكأنك خارجة من صالون تجميل 💖"
+  },
+  {
+    images: [
+      "https://media.taager.com/2ca59b80-ebe0-4bbd-a61b-3649b288ee42.png",
+      "https://media.taager.com/e8f27a4c-1b8e-42dd-9313-5edc24c4e57f.jpg",
+      "https://media.taager.com/8efc49b9-44b6-4a7e-a9a4-30148a3d358e.jpg",
+      "https://media.taager.com/e1ffa616-7211-4281-97ed-70267fd22eae.jpeg"
+    ],
+    name: "شاحن بطاريات + مجموعة بطاريات",
+    price: 350,
+    category: "إلكترونيات",
+    description: "خليك دايمًا جاهز مع شاحن البطاريات + مجموعة بطاريات.. وفر فلوسك واستعمل بطارياتك من غير ما تشتري الجديد كل شوية! ⚡"
+  },
+  {
+    images: [
+      "https://media.taager.com/aa67beed-0895-4fbc-a8b4-3ef34d98d692.jpg",
+      "https://media.taager.com/59162039-6656-4ed8-9dfe-5a42460b266d.jpg",
+      "https://media.taager.com/9591af95-8319-4ed2-9805-505e47c6b38a.jpg",
+      "https://media.taager.com/e74075df-be2e-4213-93e3-afbf1e4e34d1.jpg"
+    ],
+    name: "شنطة حرارية لحفظ الطعام 14 لتر",
+    price: 250,
+    category: "ادوات منزلية",
+    description: "شنطة تبريد حرارية بسعة 14 لتر، مثالية لحفظ الأكل و المشروبات باردة و منعشة وانت في مشوارك."
+  },
+  {
+    images: [
+      "https://media.taager.com/87b3338f-606a-4a2d-80ed-0c817bb3f657.png",
+      "https://media.taager.com/274d2daf-6f0e-4818-9031-749c9d606205.jpg",
+      "https://media.taager.com/2ac863b6-ae56-4b18-b629-179f21acc506.jpg",
+      "https://media.taager.com/1017bc9c-ca45-47f1-912a-cb71fd874a33.jpg"
+    ],
+    name: "مج حراري للقهوه",
+    price: 300,
+    category: "ادوات منزلية",
+    description: "لو من عشاق القهوة وبتحبها سخنة طول الوقت، فالمج الحراري ده هيحافظ على مشروبك دافي لأطول فترة بكل أناقة وراحة 🧡"
+  },
+  {
+    images: [
+      "https://media.taager.com/6e1dc94d-0d57-4c83-8ed1-00582f6f4803.jpg",
+      "https://media.taager.com/572e8c22-6c5c-443f-9ecd-16d4a1930c68.jpg",
+      "https://media.taager.com/91f9f6d5-d205-41fc-a9d0-f01e15dff9be.jpg",
+      "https://media.taager.com/47bdce20-5157-4288-a11b-cd9161b1b7f2.jpg"
+    ],
+    name: "كريم تنظيف الاحذية",
+    price: 200,
+    category: "منتجات تنظيف",
+    description: "كريم تنظيف أحذية سحري بيشيل البقع والأوساخ من غير مية، بيفتح اللون فورًا ويرجع الجزمة كأنها جديدة، آمن على كل الخامات وسهل الاستخدام بإسفنجة مدمجة وسريع الجفاف 👟✨"
+  },
+  {
+    images: [
+      "https://media.taager.com/e9ddf592-fd82-4e42-96da-c0770e9f6576.png",
+      "https://media.taager.com/6faadf9c-d4ee-48c2-a6f3-ee4aa188fb4b.jpeg",
+      "https://media.taager.com/950e3f88-51c0-4fac-9792-c8a4539942e0.png",
+      "https://media.taager.com/7b02c617-9bc5-4e28-b855-3edf7c5b139c.png"
+    ],
+    name: "جهاز تشغيل العاب الفيديو",
+    price: 350,
+    category: "إلكترونيات",
+    description: "جهاز ألعاب فيديو صغير وسهل الاستخدام بيجمع ألعاب زمان وبيخليك تستمتع بيها في أي وقت وفي أي مكان 🎮✨"
   }
 ];
 
